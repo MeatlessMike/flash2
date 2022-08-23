5436c42f8c744875013aa14509188c81c4577453
Akivah <akivahlaviolette@gmail.com>
2022-08-23 10:16:01 -0600

    Initial Commit

---
+++ flash2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flash2
+//
+//  Created by Laviolette, Akivah - Student on 8/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
